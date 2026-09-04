35278309e050c74a3f6461bab80b6be2cc8b8444
SinghShivendra1 <shivendra3497@gmail.com>
2026-09-04 07:35:28 +0530

    Half Diamond Star Pattern

--- patterns/pattern10.cpp
+++ patterns/pattern10.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 void pattern10(int n){
     for(int i=0; i<n; i++){
-        for(int j=0; j<i; j++){
+        for(int j=0; j<i+1; j++){
             cout<<"*";
         }
        cout<<'\n';   
     } 
+    for(int i=0; i<n; i++){
+        for(int j=n-i-1; j>0; j--){
+            cout<<"*";
+        }
+       cout<<'\n';   
+    }
+
 }
 
 int main(){
